82873a007f9bb0999d0f8e79e8b2074dc52ea9f4
Thiên Phúc <90761413+shadowfax611@users.noreply.github.com>
2021-09-23 16:29:56 +0700

    Add files via upload

---
+++ top1.cpp
@@ -0,0 +1,107 @@
+#ifdef _WIN32
+#include <conio.h>
+#include <stdio.h>
+#define clj rscr() printf("\e[1;1H\e[2J")
+#endif
+
+char square[10] = {'o', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+int checkwin()
+{
+    if (square[1] == square[2] && square[2] == square[3])
+        return 1;
+    else if (square[4] == square[5] && square[5] == square[6])
+        return 1;
+    else if (square[7] == square[8] && square[8] == square[9])
+        return 1;
+    else if (square[1] == square[4] && square[4] == square[7])
+        return 1;
+    else if (square[2] == square[5] && square[5] == square[8])
+        return 1;
+    else if (square[3] == square[6] && square[6] == square[9])
+        return 1;
+    else if (square[1] == square[5] && square[5] == square[9])
+        return 1;
+    else if (square[3] == square[5] && square[5] == square[7])
+        return 1;
+    else if (square[1] != '1' && square[2] != '2' && square[3] != '3' && square[4] != '4' && square[5] != '5' && square[6] != '6' && square[7] != '7' && square[8] != '8' && square[9] != '9')
+        return 0;
+    else
+        return -1;
+}
+void board()
+{
+    clrscr();
+    printf("\n\n\t    Tic Tac Toe\n\n");
+    printf("Nguoi choi 1 (X)  -  Nguoi choi 2 (O)\n\n\n");
+    printf("\t _________________\n");
+    printf("\t|     |     |     |\n");
+    printf("\t|  %c  |  %c  |  %c  |\n", square[1], square[2], square[3]);
+    printf("\t|_____|_____|_____|\n");
+    printf("\t|     |     |     |\n");
+    printf("\t|  %c  |  %c  |  %c  |\n", square[4], square[5], square[6]);
+    printf("\t|_____|_____|_____|\n");
+    printf("\t|     |     |     |\n");
+    printf("\t|  %c  |  %c  |  %c  |\n", square[7], square[8], square[9]);
+    printf("\t|_____|_____|_____|\n\n");
+}
+int main()
+{
+    int player = 1, i, choice;
+    char mark;
+    do
+    {
+        board();
+        if (player % 2 == 1)
+        {
+            player = 1;
+        }
+        else
+        {
+            player = 2;
+        }
+        printf("Nguoi choi %d, hay chon 1 o:\n", player);
+        scanf("%d", &choice);
+        if (player == 1)
+        {
+            mark = 'X';
+        }
+        else
+        {
+            mark = 'O';
+        }
+        if (choice == 1 && square[1] == '1')
+            square[1] = mark;
+        else if (choice == 2 && square[2] == '2')
+            square[2] = mark;
+        else if (choice == 3 && square[3] == '3')
+            square[3] = mark;
+        else if (choice == 4 && square[4] == '4')
+            square[4] = mark;
+        else if (choice == 5 && square[5] == '5')
+            square[5] = mark;
+        else if (choice == 6 && square[6] == '6')
+            square[6] = mark;
+        else if (choice == 7 && square[7] == '7')
+            square[7] = mark;
+        else if (choice == 8 && square[8] == '8')
+            square[8] = mark;
+        else if (choice == 9 && square[9] == '9')
+            square[9] = mark;
+        else
+        {
+            printf("Nuoc di nay khong hop le!");
+            player--;
+            getch();
+        }
+        i = checkwin();
+        player++;
+    } while (i == -1);
+    board();
+    if (i == 1)
+        printf("Chuc mung nguoi choi %d gianh chien thang", --player);
+    else
+        printf("Game hoa!");
+    getch();
+    return 0;
+}
